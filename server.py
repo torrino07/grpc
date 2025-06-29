941fb38661319f0428d4cdd7ad3dd7c2207b4f8e
DGtalledo <61323407+torrino07@users.noreply.github.com>
2025-06-29 20:40:40 +0200

    test

--- server.py
+++ server.py
@@ -64,7 +64,7 @@ class CommandExecutorServicer(command_pb2_grpc.CommandExecutorServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     command_pb2_grpc.add_CommandExecutorServicer_to_server(CommandExecutorServicer(), server)
-    server.add_insecure_port("[::]:50053")
+    server.add_insecure_port("[::]:50052")
     server.start()
     server.wait_for_termination()
 
